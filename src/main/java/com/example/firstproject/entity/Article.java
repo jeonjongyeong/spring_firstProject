5761c74d2874cc2ea8c36ee0281e46c927df5e97
jkkjkhh <jkkjkhh@email.com>
2022-07-11 22:11:54 +0900

    링크와 리다이렉트를 사용하여 게시물간 이동

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Getter
 public class
 Article {
 
@@ -25,5 +27,6 @@ Article {
 
     @Column
     private String content;
+
 
 }

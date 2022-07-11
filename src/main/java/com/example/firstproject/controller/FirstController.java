5b1e3105d311a4cfa12776090487ebc7e3783855
jkkjkhh <jkkjkhh@email.com>
2022-07-11 15:00:45 +0900

    MVC를 활용한 greeting, bye 페이지 만들기

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,4 +1,21 @@
 package com.example.firstproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "종영형");
+        return "greetings"; // templates/greetings.mustache 파일을 찾아서 브라우저로 전송
+    }
+
+    @GetMapping("/bye")
+    public String seeYou(Model model) {
+        model.addAttribute("nickname", "종영");
+        return "goodbye";
+    }
 }

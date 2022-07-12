ee222fc9d66ac8dbdccda9ea2bae43eecaeb377b
jkkjkhh <jkkjkhh@email.com>
2022-07-12 10:14:10 +0900

    데이터 수정하기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -80,4 +80,27 @@ public class ArticleController {
         // 뷰 페이지 설정
         return "articles/edit";
     }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        log.info(form.toString());
+
+        // 1. DTO를 엔티티로 변환한다.
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+
+        // 2. 엔티티를 DB로 저장한다.
+        //  2-1. DB에 기존 데이터를 가져온다
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+
+        //  2-2. 기존 데이터에 값을 수정
+        if (target != null) {
+            articleRepository.save(articleEntity); // 엔티티가 DB 갱신
+        }
+
+
+        // 3. 수정 결과를 페이지로 리다이렉트 한다.
+        return "redirect:/articles/" + articleEntity.getId();
+    }
 }

1f41d6ff88ca033d863af3faf0865c1216f6f12a
jkkjkhh <jkkjkhh@email.com>
2022-07-14 22:31:03 +0900

    comment 댓글 삭제 기능 추가

--- src/main/java/com/example/firstproject/api/CommentApiController.java
+++ src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -49,4 +49,16 @@ public class CommentApiController {
     }
 
     // 댓글 삭제
+    @DeleteMapping("/api/articles/comments/{id}")
+    public ResponseEntity<CommentDto> delete(@PathVariable Long id) {
+
+
+        // 서비스에게 위임
+        CommentDto updatedDto = commentService.delete(id);
+
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(updatedDto);
+    }
+
+
 }

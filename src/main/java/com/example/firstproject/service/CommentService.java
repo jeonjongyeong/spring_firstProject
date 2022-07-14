8a3860793d21d243c5403a11451facd3be42d9c9
jkkjkhh <jkkjkhh@email.com>
2022-07-14 13:44:32 +0900

    comment 댓글 생성 기능 추가

--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -1,10 +1,13 @@
 package com.example.firstproject.service;
 
 import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.entity.Comment;
 import com.example.firstproject.repository.ArticleRepository;
 import com.example.firstproject.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -36,4 +39,24 @@ public class CommentService {
                 .map(comment -> CommentDto.createCommentDto(comment))
                 .collect(Collectors.toList());
     }
+
+    @Transactional
+    public CommentDto create(Long articleId, CommentDto dto) {
+        // 게시글 조회 및 예외 발생
+        Article article = articleRepository.findById(articleId).orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패! 대상 게시글이 없습니다."));
+
+
+        // 댓글 엔티티 생성
+        Comment comment = Comment.createComment(dto, article);
+
+        // 댓글 엔티티를 DB로 저장
+        Comment created = commentRepository.save(comment);
+
+        // Dto로 변경하여 반환
+
+        return CommentDto.createCommentDto(created);
+
+
+    }
+
 }
